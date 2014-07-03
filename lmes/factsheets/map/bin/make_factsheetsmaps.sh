6ee52608110e9182ead6059c7bd951d7abb9236e
GeowowRoot <root@geowow.iode.org>
2014-07-03 09:50:13 +0200

    corrected problem with LMEs layer for antarctic

--- lmes/factsheets/map/bin/make_factsheetsmaps.sh
+++ lmes/factsheets/map/bin/make_factsheetsmaps.sh
@@ -2,7 +2,7 @@
 
 # author: Bruno Combal
 # date: September 2013
-# purpose: read original data and create well-formated data files and create html template files
+# purpose: read original data and create well-formatted data files and create html template files
 
 template='lme_centered_template.html'
 polarNorthTemplate='lmes_centered_polar_template.html'
@@ -11,7 +11,7 @@ outiframe='../iframe'
 csv="/data/private_store/lmes/lmes66_data/lmes_factsheet.csv"
 mkdir -p $outiframe
 
-declare -A nodeCodes=(["0"]=66 ["1"]="51" ["2"]="55" ["3"]="56" ["4"]="57" ["5"]="58" ["6"]="59" ["7"]="60" ["8"]="61" ["9"]="62" ["10"]="63" ["11"]="65" ["12"]="66" ["13"]="67" ["14"]="68" ["15"]="69" ["16"]="70" ["17"]="71" ["18"]="72" ["19"]="73" ["20"]="74" ["21"]="75" ["22"]="76" ["23"]="77" ["24"]="78" ["25"]="79" ["26"]="80" ["27"]="81" ["28"]="82" ["29"]="83" ["30"]="84" ["31"]="85" ["32"]="86" ["33"]="87" ["34"]="88" ["35"]="89" ["36"]="90" ["37"]="91" ["38"]="92" ["39"]="93" ["40"]="94" ["41"]="95" ["42"]="96" ["43"]="97" ["44"]="98" ["45"]="99" ["46"]="100" ["47"]="101" ["48"]="102" ["49"]="103" ["50"]="104" ["51"]="105" ["52"]="106" ["53"]="107" ["54"]="108" ["55"]="109" ["56"]="110" ["57"]="111" ["58"]="112" ["59"]="113" ["60"]="114" ["61"]="115" ["62"]="116" ["63"]="117" ["64"]="118" ["65"]="119" ["66"]="120")
+declare -A nodeCodes=(["0"]="120" ["1"]="51" ["2"]="55" ["3"]="56" ["4"]="57" ["5"]="58" ["6"]="59" ["7"]="60" ["8"]="61" ["9"]="62" ["10"]="63" ["11"]="65" ["12"]="66" ["13"]="67" ["14"]="68" ["15"]="69" ["16"]="70" ["17"]="71" ["18"]="72" ["19"]="73" ["20"]="74" ["21"]="75" ["22"]="76" ["23"]="77" ["24"]="78" ["25"]="79" ["26"]="80" ["27"]="81" ["28"]="82" ["29"]="83" ["30"]="84" ["31"]="85" ["32"]="86" ["33"]="87" ["34"]="88" ["35"]="89" ["36"]="90" ["37"]="91" ["38"]="92" ["39"]="93" ["40"]="94" ["41"]="95" ["42"]="96" ["43"]="97" ["44"]="98" ["45"]="99" ["46"]="100" ["47"]="101" ["48"]="102" ["49"]="103" ["50"]="104" ["51"]="105" ["52"]="106" ["53"]="107" ["54"]="108" ["55"]="109" ["56"]="110" ["57"]="111" ["58"]="112" ["59"]="113" ["60"]="114" ["61"]="115" ["62"]="116" ["63"]="117" ["64"]="118" ["65"]="119" ["66"]="120")
 
 # list of templates
 declare -A LMETemplate
@@ -67,31 +67,32 @@ do
     # replace countries
     echo "RRRRRR countries"$countries
     if [ -z "${countries}" ]; then
-	sed -i 's/COUNTRYTOREPLACE/No Country/' ${outname}
+		sed -i 's/COUNTRYTOREPLACE/No Country/' ${outname}
     else
-	plural=0
-	plural=$(echo ${countries} | grep ',' | wc -l)
-	echo "PLURAL "$plural
-	if [ $plural -eq 0 ]; then
-	    countries="\<b\>Country:\<\/b\> "${countries}
-	else
-	    countries="\<b\>Countries:\<\/b\> "${countries}
+		plural=0
+		plural=$(echo ${countries} | grep ',' | wc -l)
+		echo "PLURAL "$plural
+		if [ $plural -eq 0 ]; then
+			countries="\<b\>Country:\<\/b\> "${countries}
+		else
+			countries="\<b\>Countries:\<\/b\> "${countries}
+		fi
+
+		perl -i -pe "s/COUNTRYTOREPLACE/${countries}/" ${outname}
 	fi
+	# replace WIDTH and HEIGHT
+	widthVal=600
+	heightVal=300
+	#exceptions
+	case $ii in
+		29) widthVal=200; heightVal=380;;
+		30) widthVal=600; heightVal=350;;
+		35) widthVal=300; heightVal=600;;
+		61) widthVal=600; heightVal=400;;
+	esac
+	echo ${widthVal}  ${outname}
+	sed -i 's/WIDTHTOREPLACE/'${widthVal}'/' ${outname}
+	sed -i 's/HEIGHTTOREPLACE/'${heightVal}'/' ${outname}
 
-	perl -i -pe "s/COUNTRYTOREPLACE/${countries}/" ${outname}
-
-    # replace WIDTH and HEIGHT
-    widthVal=600
-    heightVal=300
-    #exceptions
-    case $ii in
-	29) widthVal=200; heightVal=380;;
-	30) widthVal=600; heightVal=350;;
-	35) widthVal=300; heightVal=600;;
-    esac
-    sed -i 's/WIDTHTOREPLACE/'${widthVal}'/' ${outname}
-    sed -i 's/HEIGHTTOREPLACE/'${heightVal}'/' ${outname}
-
-    fi
 done
 
