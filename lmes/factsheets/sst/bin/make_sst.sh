ea6a2b5ec4f4def382d03ab3d4a21b2a631a351e
GeowowRoot <root@geowow.iode.org>
2014-12-22 15:01:31 +0100

    re-indented, corrected a typo in the legend for the trend line

--- lmes/factsheets/sst/bin/make_sst.sh
+++ lmes/factsheets/sst/bin/make_sst.sh
@@ -15,7 +15,7 @@ do
     cp $out/'bin'/${template} ${outname}
     # edit in place
     name=$(awk -v code=${ii} '{if ($1==code) print $2}' ${lmeCode} | sed 's/[0-9]*\ //' | tr '[:upper:]' '[:lower:]')
-	lmeName=$(echo ${name} | sed 's/_/ /g' | sed -e "s/\b\(.\)/\u\1/g" | sed -s "s/ Us / U.S. /g")
+    lmeName=$(echo ${name} | sed 's/_/ /g' | sed -e "s/\b\(.\)/\u\1/g" | sed -s "s/ Us / U.S. /g")
     echo $ii'... ready!'
     perl -i -pe 's/LMENAMETOREPLACE/'"${lmeName}"'/' ${outname}
     sed -i 's/LMECODETOREPLACE/'${ii}'/' ${outname}
@@ -27,9 +27,9 @@ f=$outdir/temp
 data=$(awk -F ' ' '{printf "%02d\n", $1;}' '/data/iframes/lmes/factsheets/sst/data/sst_data_trend.txt' | sort -n | uniq | grep -v 99)
 for lmeNumber in $data
 do
-	name=$(awk -F ' ' -v thisLME=${lmeNumber} '{if ($1==thisLME) {printf "%s", $2};}' $mapfile | tr '[:upper:]' '[:lower:]')
-	lmeName=$(echo ${name} | sed 's/_/ /g' | sed -e "s/\b\(.\)/\u\1/g" | sed -s "s/ Us / U.S. /g")
-	echo $lmeNumber','$lmeName >> $f
+    name=$(awk -F ' ' -v thisLME=${lmeNumber} '{if ($1==thisLME) {printf "%s", $2};}' $mapfile | tr '[:upper:]' '[:lower:]')
+    lmeName=$(echo ${name} | sed 's/_/ /g' | sed -e "s/\b\(.\)/\u\1/g" | sed -s "s/ Us / U.S. /g")
+    echo $lmeNumber','$lmeName >> $f
 done
 
 arrayLMEs="$arrayLMEs $(awk -F ',' '{printf "\"%s %s\",\n", $1, $2}' $f)"
@@ -40,8 +40,8 @@ unlink $f
 
 for f in ${outdir}/*.php
 do
-	perl -i -pe 's/LISTOFAVAILABLELMES/ '"${arrayLMEs}"'/' ${f}
- done
+    perl -i -pe 's/LISTOFAVAILABLELMES/ '"${arrayLMEs}"'/' ${f}
+done
 
 
 echo $code'SST ... DONE!'
