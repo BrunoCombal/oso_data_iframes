a602f87bb731263db85973c75dec75cac9ba3828
GeowowRoot <root@geowow.iode.org>
2015-04-03 14:46:16 +0200

    added WPWP, 99

--- lmes/factsheets/coral/bin/make.sh
+++ lmes/factsheets/coral/bin/make.sh
@@ -7,7 +7,7 @@ template=template.php
 out=/data/iframes/lmes/factsheets/coral/iframes
 mkdir -p ${out}
 
-listLME=(4 5 6 10 11 12 16 17 30 31 32 33 34 35 36 37 38 39 40 41 44 45 47 49)
+listLME=(4 5 6 10 11 12 16 17 30 31 32 33 34 35 36 37 38 39 40 41 44 45 47 49 99)
 for ii in ${listLME[@]}
 do
     outfile=${out}/coral_${ii}.php
