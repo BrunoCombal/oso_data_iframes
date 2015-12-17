aa27c1426f9fcbf0e96d1d1ef91e942214d00e20
GeowowRoot <root@geowow.iode.org>
2015-12-17 18:02:01 +0100

    init

---
+++ common/landing/landing.php
@@ -0,0 +1,130 @@
+<style>
+ @import url(https://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic);
+ @import url(https://fonts.googleapis.com/css?family=Muli:400,700,400italic,700italic);
+ @font-face{
+   font-family: "Alleyn-Light";
+   src: url("/iframes/fonts/Alleyn_Family/Alleyn-Light.otf") format("opentype");
+ }
+ @font-face{
+   font-family: "Alleyn-Regular";
+   src: url("/iframes/fonts/Alleyn_Family/Alleyn-Regular.otf") format("opentype");
+ }
+
+ body {
+   background: url("/iframes/common/landing/12197898755_b3c85b617a_k_d_edited.jpg") no-repeat center center fixed;
+   -webkit-background-size: cover;
+   -moz-background-size: cover;
+   -o-background-size: cover;
+   background-size: cover;
+   color:#0e0e0e;
+   font-family:"Alleyn-Light", "Muli", "PT sans", sans-serif;
+ }
+ .announce {
+   font-family:"Alleyn-Regular", "PT sans", sans-serif;
+   font-size:22px;
+   text-align:justified!important;
+   margin:0 auto;
+   line-height:1.4em;
+
+ }
+ .announce h1{
+   font-size:32px;
+   font-weight:bold;
+   text-align:center;
+   color:#FFF;
+   margin-bottom:0;
+   margin-top:10px;
+ }
+ .announce h2{
+   font-family:"Alleyn-Light", Muli;
+   font-weight:normal;
+   font-size:64px;
+   color:#fff;
+   margin:0;
+   padding:0;
+   text-align:center!important;;
+ }
+ h3{
+   position:relative;
+   top:350px;
+   right:-100px;
+   font-family:"PT sans", Verdana, sans-serif;
+   font-size:20px;
+   font-weight:normal;
+   text-align:center;
+   color:#fff;
+ }
+ .imagecredit{
+   font-family:"PT sans";
+   font-size:10px;
+   float:right;
+   position:relative;
+   left:-20px;
+   color:#ccc;
+   margin-top:100px;
+ }
+ .imagecredit a{
+   color:#ccc;
+ }
+ .footer{
+   font-family: "PT sans", "sans-serif";
+ }
+</style>
+
+
+<div class="announce" style="width:920px; margin 0; padding:0">
+  <!--
+  <h1>We are all sharing the same ocean...</h1>
+  However, human activity is not uniform across the globe,<br/>
+  incurring variable global and local risk on ocean services.<br/>
+  Discover details for <a href="//onesharedocean.org/open_ocean" target="_top" style="font-weight:bold; ">Open Ocean</a> and <a href="//onesharedocean.org/lmes" target="_top" style="font-weight:bold;">Large Marine Ecosystems</a><br/>
+  -->
+  <h2>One Shared Ocean</h2>
+  <div id="canvas" style=" margin:0px auto; padding:0; ">
+    <div style="float:left; font-weight:bold; font-size:24px; padding-bottom:1em"><a href="/open_ocean" style="color:#fff">Open Ocean</a></div> <div style="float:right; font-weight:bold; font-size:24px; padding-bottom:1em"><a href="/lmes" style="color:#fff">Large Marine Ecosystems</a></div>
+    <div style="clear:both"></div>
+    <div style="background: rgba(230, 244, 253, 0.7); padding:15px;">
+      The Earth’s marine systems provide multiple ecosystem goods and services essential for human well-being and socio-economic development. Undeniable trends, however, indicate that the health and productivity of these systems are being degraded by multiple and complex anthropogenic and natural stressors.<br/>
+      The GEF-funded Transboundary Waters Assessment Programme (<a href="http://www.geftwap.org/">TWAP</a>) is assessing transboundary aquifers, reservoirs and lakes, river basins, Large Marine Ecosystems (LMEs) and the open ocean to guide interventions to improve the environment and provide benefits for human well-being.<br/>
+      The TWAP marine assessments were led by the Intergovernmental Oceanographic Commission of UNESCO:
+      <ul style="padding-top:0; margin-top:0; margin-bottom:0; padding-bottom:0">
+        <li>The <a href="/open_ocean">Open Ocean</a> is the largest of the planet’s transboundary water spaces and is the medium for many global issues with local impacts</li>
+        <li>The 66 <a href="/lmes">Large Marine Ecosystems</a> encompass the Earth’s coastal areas where many ecosystem services and human stressors are concentrated</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div style="clear:both"></div>
+<div class="footer" style="position:fixed; bottom:0px; width:960px; margin:0 auto; padding:0; ">
+  <div class="imagecredit">
+    Original background image by <a href="//www.flickr.com/photos/usfwspacific/12197898755/" target="_blank">Susan White/USFWS</a> <a href="//creativecommons.org/licenses/by/2.0/" target="_blank">CC BY 2.0</a>
+  </div>
+  <div style="clear:both"></div>
+  <div id="footer_logos" style="background-color:#fff; padding-top:4px; padding-left:4px;">
+    <span>
+      <!-- UNESCO/IOC logo -->
+      <a href="//ioc.unesco.org" target="blank" ><img src="/iframes/iconz/unesco_ioc_small.png" style="background-color:#fff" width="100px" align="top"></img></a>
+      <!-- EU logo -->
+      <a href="//europa.eu" target="blank"><img src="/iframes/iconz/eu_flag_color.jpg"  align="top" width="60px" ></img></a>
+      <!-- GEO logo -->
+      <a href="//www.earthobservations.org" target="blank"><img src="/iframes/iconz/geo_logo.png" align="top" width="100px"></img></a>
+      <!-- GEF logo -->
+      <a href="//thegef.org" target="blank"><img src="/iframes/iconz/gef_logo_small.png" width="60px" align="top"></img></a>
+      <!-- UNEP logo -->
+      <a href="//unep.org"><img src="/iframes/iconz/unep.png" style="background-color:#fff" width="55px" align="top"></img></a>
+      <!-- TWAP logo -->
+      <a href="//www.geftwap.org" target="blank"><img src="/iframes/iconz/TWAP-Logo.png" style="background-color:#fff" width="180px" align="top"></img></a>
+    </span>
+    <div id="credits" style="background-color:#fff">
+      <span id="copyright" >&copy;&nbsp;<?php print(date('Y') . ' ');?>
+        <?php if (!empty($site_name)):?>
+          <?php print $site_name;?>, IOC-UNESCO
+        <?php endif;?>
+      </span>
+      “This project has received funding from the European Union’s Seventh Programme for research, technological development and demonstration under grant agreement No 282915”. <br/><a href="http://www.geowow.eu/" target="_blank">www.geowow.eu</a>
+    </div>
+  </div>
+</div>
+
+
